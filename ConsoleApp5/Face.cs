addcd9ab41f3d3d75339a7576a2a282e98da2e8d
Yerireyes <le_soleto@hotmail.com>
2021-11-07 00:19:43 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp5/Face.cs
@@ -0,0 +1,99 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Face
+    {
+        public Dictionary<string, Vector> listaDeVertices { get; set; }
+        public Color color { get; set; }
+        public Vector centro { get; set; }
+        public Vector traslacion { get; set; } = new Vector(0, 0, 0);
+
+        public Face(Dictionary<string, Vector> listaDeVertices, Color color, Vector centro)
+        {
+            this.listaDeVertices = listaDeVertices;
+            this.color = color;
+            this.centro = centro;
+        }
+
+        public void Insertar(Vector nuevoVertice, string key)
+        {
+            listaDeVertices.Add(key, nuevoVertice);
+        }
+
+        public void Eliminar(string key)
+        {
+            listaDeVertices.Remove(key);
+        }
+
+        public Vector Obtener(string key)
+        {
+            return listaDeVertices[key];
+        }
+
+        public void Dibujar()
+        {
+            GL.Color4(color);
+            GL.Begin(PrimitiveType.Polygon);
+
+            foreach (var vertice in listaDeVertices)
+            {
+                GL.Vertex3(vertice.Value.X + centro.X + traslacion.X, vertice.Value.Y + centro.Y + traslacion.Y, vertice.Value.Z + centro.Z + traslacion.Z);
+
+            }
+            GL.End();
+
+        }
+
+        public void Rotar(float anguloX, float anguloY, float anguloZ)
+        {
+            anguloX = MathHelper.DegreesToRadians(anguloX);
+            anguloY = MathHelper.DegreesToRadians(anguloY);
+            anguloZ = MathHelper.DegreesToRadians(anguloZ);
+            foreach (var vertice in listaDeVertices)
+            {
+                vertice.Value.Set(RotarX(vertice.Value, anguloX));
+                vertice.Value.Set(RotarY(vertice.Value, anguloY));
+                vertice.Value.Set(RotarZ(vertice.Value, anguloZ));
+            }
+        }
+
+        private Vector RotarX(Vector verticeARotar, float angulo)
+        {
+            Matrix3 matriz = Matrix3.CreateRotationX(angulo);
+            return verticeARotar * matriz;
+        }
+
+        private Vector RotarY(Vector verticeARotar, float angulo)
+        {
+            Matrix3 matriz = Matrix3.CreateRotationY(angulo);
+            return verticeARotar * matriz;
+        }
+
+        private Vector RotarZ(Vector verticeARotar, float angulo)
+        {
+            Matrix3 matriz = Matrix3.CreateRotationZ(angulo);
+            return verticeARotar * matriz;
+        }
+
+        public void Trasladar(float x, float y, float z)
+        {
+            traslacion += new Vector(x, y, z);
+        }
+
+        public void Escalado(float x, float y, float z)
+        {
+            Matrix3 matriz = Matrix3.CreateScale(x, y, z);
+            foreach (var vertice in listaDeVertices)
+            {
+                vertice.Value.Set(vertice.Value * matriz);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp5/Game.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using System.Drawing;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace ConsoleApp5
+{
+    class Game : GameWindow
+    {
+        private Dictionary<String, Objeto3D> objetos = new Dictionary<String, Objeto3D>();
+        private Dictionary<String, Objeto3D> objetos1 = new Dictionary<String, Objeto3D>();
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
+        public HelloEscenario escenario;
+        public HelloEscenario escenario2;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(Color4.White);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.Ortho(-500, 500, -500, 500, -500, 500);
+            Objeto3D prueba = CargadorJson.Cargar("../../Objetos/prueba.json");
+            Objeto3D prueba1 = CargadorJson.Cargar("../../Objetos/prueba1.json");
+            Objeto3D prueba2 = CargadorJson.Cargar("../../Objetos/prueba.json");
+            Objeto3D prueba3 = CargadorJson.Cargar("../../Objetos/prueba1.json");
+            objetos.Add("prueba", prueba);
+            objetos.Add("prueba1", prueba1);
+            objetos1.Add("prueba2", prueba2);
+            objetos1.Add("prueba3", prueba3);
+            escenario = new HelloEscenario(objetos, new Vector(-200, -100, 0));
+            escenario2 = new HelloEscenario(objetos1, new Vector(50, 50, 0));
+
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.MatrixMode(MatrixMode.Modelview);
+            escenario.Dibujar();
+            escenario.Obtener("prueba").Rotar(1, 0, 0);
+            escenario2.Dibujar();
+            SwapBuffers();
+
+            base.OnRenderFrame(e);
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+    }
+}
